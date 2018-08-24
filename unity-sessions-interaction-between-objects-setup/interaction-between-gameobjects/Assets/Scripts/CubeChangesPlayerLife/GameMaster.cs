960f41a364594d5d87634f5abdf6e3fac296ad01
Emiliano Pineda <emi.rock11@hotmail.com>
2018-08-24 18:44:29 -0500

    Agregé las dos actividades requeridas

---
+++ unity-sessions-interaction-between-objects-setup/interaction-between-gameobjects/Assets/Scripts/CubeChangesPlayerLife/GameMaster.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CubeChangesPlayerLife
+{
+	public class GameMaster : MonoBehaviour {
+        
+        public Player Sphere;
+        // Use this for initialization
+
+        void OnMouseDown()
+        {
+            Sphere.life--;
+        }
+    }
+}
---
+++ unity-sessions-interaction-between-objects-setup/interaction-between-gameobjects/Assets/Scripts/CubeChangesPlayerLife/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public int life = 10;
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    void OnGUI()
+    {
+        GUILayout.Label("LP: " + life);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
